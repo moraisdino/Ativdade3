83fa0940e82d9221403472add91a5de3d1d756a6
José Gabriel Morais <169068802+moraisdino@users.noreply.github.com>
2024-09-02 12:24:00 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+    int = 1
+    int = 2
+    int = 3
+    int = 4
+
+     }
+
+
+    }
